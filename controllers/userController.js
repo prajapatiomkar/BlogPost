dc8279051e8fe4c5ef360744ca4cc451b3123618
Prajapati omkar <prajapatiomkar01@gmail.com>
2022-09-22 21:08:19 +0530

    Successfully implemented login api

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,12 +3,21 @@ const { body, validationResult } = require('express-validator');
 const User = require("../models/User")
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-module.exports.registerValidation = [
+
+const createToken = (user) => {
+    return jwt.sign({ user }, process.env.SECRET, {
+        expiresIn: "7d"
+    });
+}
+
+module.exports.registerValidations = [
     body("name").not().isEmpty().trim().withMessage("Name is required"),
     body("email").not().isEmpty().trim().withMessage("Email is required"),
     body("password").isLength({ min: 6 }).withMessage("Password must be 6 character long")
 
 ];
+
+// Register Module
 module.exports.register = async (req, res) => {
     const { name, email, password } = req.body;
     const errors = validationResult(req);
@@ -30,9 +39,7 @@ module.exports.register = async (req, res) => {
                 email,
                 password: hash,
             });
-            const token = jwt.sign({ user }, process.env.SECRET, {
-                expiresIn: "7d"
-            });
+            const token = createToken(user);
             return res.status(200).json({ msg: "Your account has been created", token })
         } catch (error) {
             return res.status(500).json({ errors: error })
@@ -40,4 +47,40 @@ module.exports.register = async (req, res) => {
     } catch (error) {
         return res.status(500).json({ errors: error })
     }
+}
+
+// Login Module
+
+module.exports.loginValidations = [
+    body("email").not().isEmpty().withMessage("Email is required"),
+    body("password").not().isEmpty().withMessage("Password is  required"),
+
+];
+module.exports.login = async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+    const { email, password } = req.body;
+    try {
+        
+
+        const user = await User.findOne({ email });
+        if (user) {
+            const matched = await bcrypt.compare(password, user.password)
+            if (matched) {
+                const token = createToken(user);
+                return res.status(200).json({ msg: "You have login successfully", token })
+            } else {
+                return res.status(401).json({ errors: [{ msg: "password is not correct" }] }) //401- Password doesn't match
+            }
+
+        } else {
+            return res.status(404).json({ errors: [{ msg: "Email not found" }] }) //404-Not found
+        }
+    } catch (error) {
+        return res.status(500).json({ errors: error }) //server error
+    }
+
+    
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,11 @@
 const app = require("express");
 const router = app.Router();
-const { register,registerValidation } = require("../controllers/userController")
-router.post("/register",registerValidation, register);
+const {
+    register,
+    registerValidations,
+    login,
+    loginValidations
+} = require("../controllers/userController")
+router.post("/register", registerValidations, register);
+router.post("/login",loginValidations,login)
 module.exports = router;
